5361fe413304ac276ea238fcfeaa290daca2dcba
Skylar Kenneth <azva_s_kenneth@yahoo.com>
2023-03-02 18:29:28 -0700

    Add project files.

---
+++ DbContextSaveChangesResolver/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace DbContextSaveChangesResolver
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ SaveChangesResolverTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SaveChangesResolverTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
