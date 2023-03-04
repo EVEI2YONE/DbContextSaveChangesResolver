1fe6406977dde8916140e918ee028a49c013efc8
Skylar Kenneth <azva_s_kenneth@yahoo.com>
2023-03-04 16:58:50 -0700

    Create dependency resolver to extrapolate Types that are least dependent for execution order

--- DbContextSaveChangesResolver/Services/DependencyResolver.cs
+++ DbContextSaveChangesResolver/Services/DependencyResolver.cs
@@ -10,9 +10,32 @@ namespace DbContextSaveChangesResolver.Services
     public class DependencyResolver
     {
         private Graph Graph;
+        private List<Vertex> ExecutionOrder;
         public DependencyResolver(Graph graph)
         {
-            
+            this.Graph = graph;
+            this.ExecutionOrder = new List<Vertex>();
+        }
+
+        public void ResolveDependencies()
+        {
+            foreach (var vertex in Graph.Vertices.OrderBy(x => x.Name))
+                DijkstraTraverseAndBuildDependencies(vertex);
+        }
+
+        public void DijkstraTraverseAndBuildDependencies(Vertex vertex)
+        {
+            if (vertex == null)
+                return;
+            if (!vertex.AdjacentVertices.Any())
+                ExecutionOrder.Add(vertex);
+            foreach(var adjVertex in vertex.AdjacentVertices.OrderBy(x => x.Name))
+                DijkstraTraverseAndBuildDependencies(adjVertex);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(", ", ExecutionOrder);
         }
     }
 }
