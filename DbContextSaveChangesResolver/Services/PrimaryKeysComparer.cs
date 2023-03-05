e107ddde0909cef34293220d987ee5a0ef8c4046
Skylar Kenneth <azva_s_kenneth@yahoo.com>
2023-03-04 17:00:45 -0700

    Create Primary Key Comparer to increase performance. E.g. updating an intended object that was supposed to be inserted. Object has to be the same type with same Keys (PKs and FKs). No need to attach and use DbContext tracker

---
+++ DbContextSaveChangesResolver/Services/PrimaryKeysComparer.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbContextSaveChangesResolver.Services
+{
+    public class PrimaryKeysComparer : IEqualityComparer<object>
+    {
+        private IDictionary<Type, List<PropertyInfo>> PrimaryKeyProperties;
+
+        public PrimaryKeysComparer(IDictionary<Type, List<PropertyInfo>> PKProperties)
+        {
+            this.PrimaryKeyProperties = PKProperties;
+        }
+
+        public bool Equals(object? x, object? y)
+        {
+            return x == y || GetHashCode(x) == GetHashCode(y);
+        }
+
+        public int GetHashCode([DisallowNull] object obj)
+        {
+            int hash = 43;
+            if (PrimaryKeyProperties.ContainsKey(obj.GetType()))
+                hash = CalculateHash(obj.GetType(), hash, obj);
+            else
+                hash = CalculateHash(obj.GetType().UnderlyingSystemType, hash, obj);
+            return hash;
+        }
+
+        private int CalculateHash(Type type, int hash, object obj)
+        {
+            foreach (var property in PrimaryKeyProperties[type])
+                hash = hash * 71 + property.GetValue(obj).GetHashCode();
+            return hash;
+        }
+    }
+}
